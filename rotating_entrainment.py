5513c2835cda4a78db5689f4ea77177889dbf3b5
Evan Anders <evanhanders@gmail.com>
2022-12-19 13:48:42 -0600

    nominally sets up initial conditions

--- rotating_entrainment.py
+++ rotating_entrainment.py
@@ -7,11 +7,19 @@ import dedalus.public as d3
 import logging
 logger = logging.getLogger(__name__)
 
+#Heaviside functions
+from scipy.special import erf
+def one_to_zero(x, x0, width=0.1):
+    return (1 - erf( (x - x0)/width))/2
+
+def zero_to_one(*args, **kwargs):
+    return -(one_to_zero(*args, **kwargs) - 1)
+
 
 # Parameters
 aspect, Lz = 4, 1
-Nx, Nz = 256, 64
-Rayleigh = 2e6
+Nx, Nz = 64, 64
+Rayleigh = 1e6
 Prandtl = 0.5
 Taylor = 1
 tau = Prandtl
@@ -51,6 +59,7 @@ kappaC = tau * kappaT
 nu = (Rayleigh / Prandtl)**(-1/2)
 omega = (Taylor * Prandtl / Rayleigh)**(1/2)
 x, y, z = dist.local_grids(xbasis, ybasis, zbasis)
+x_de, y_de, z_de = dist.local_grids(xbasis, ybasis, zbasis, scales=(dealias, dealias, dealias))
 ex, ey, ez = coords.unit_vector_fields(dist)
 lift_basis = zbasis.derivative_basis(1)
 lift = lambda A: d3.Lift(A, lift_basis, -1)
@@ -68,9 +77,9 @@ C['g'] = C0 = 1 - z
 
 T0_bot = T(z=0).evaluate()
 C0_bot = C(z=0).evaluate()
+T0z_top = (ez@(d3.grad(T)(z=Lz))).evaluate()
 
 #TODO: get this from Rayleigh number
-F = -10
 
 # Problem
 # First-order form: "div(f)" becomes "trace(grad_f)"
@@ -84,7 +93,7 @@ problem.add_equation("C(z=0) = C0_bot")
 problem.add_equation("T(z=0) = T0_bot")
 problem.add_equation("u(z=0) = 0")
 problem.add_equation("ez@(grad(C)(z=Lz)) = 0")
-problem.add_equation("ez@(grad(T)(z=Lz)) = F")
+problem.add_equation("ez@(grad(T)(z=Lz)) = T0z_top")
 problem.add_equation("ez@(u(z=Lz)) = 0")
 problem.add_equation("ex@shear_stress = 0")
 problem.add_equation("ey@shear_stress = 0")
@@ -97,12 +106,29 @@ solver.stop_sim_time = stop_sim_time
 # Initial conditions
 noise = dist.Field(name='noise', bases=bases)
 noise.fill_random('g', seed=42, distribution='normal', scale=1e-3) # Random noise
+
+noise.change_scales(dealias)
+T.change_scales(dealias)
+C.change_scales(dealias)
 T['g'] += noise['g']
+
+C['g'] *= one_to_zero(z_de, 0.8, width=0.05)
+C['g'] += 0.1*zero_to_one(z_de, 0.8, width=0.05)
+
+#import matplotlib.pyplot as plt
+#plt.plot(z_de.ravel(), C['g'][0,0,:], label='C')
+#plt.plot(z_de.ravel(), T['g'][0,0,:], label='T')
+#plt.legend()
+#plt.show()
 
 # Analysis
 snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=0.25, max_writes=50)
-snapshots.add_task(T - inv_R*C, name='buoyancy')
-snapshots.add_task(-d3.div(d3.skew(u)), name='vorticity')
+snapshots.add_task((T - inv_R*C)(x=aspect/2), name='buoyancy yz')
+snapshots.add_task((T - inv_R*C)(y=aspect/2), name='buoyancy xz')
+snapshots.add_task((T - inv_R*C)(z=0.9), name='buoyancy xy')
+snapshots.add_task((ez@d3.curl(u))(x=aspect/2), name='vorticity yz')
+snapshots.add_task((ez@d3.curl(u))(y=aspect/2), name='vorticity xz')
+snapshots.add_task((ez@d3.curl(u))(z=0.9), name='vorticity xy')
 
 # CFL
 CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=10, safety=0.5, threshold=0.05,
