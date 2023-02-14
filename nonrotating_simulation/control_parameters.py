80639e58b61e00e04a7db51ba3a35ef052b48331
Rafael Fuentes <43425560+jorafb@users.noreply.github.com>
2023-02-14 08:06:50 -0700

    updating parameters

--- nonrotating_simulation/control_parameters.py
+++ nonrotating_simulation/control_parameters.py
@@ -1,17 +1,17 @@
 from collections import OrderedDict
 
 parameters = OrderedDict()
-parameters['Nz'] = 256
-parameters['Nx'] = 256
+parameters['Nz'] = 512
+parameters['Nx'] = 192
 parameters['Lz'] = 2
 parameters['aspect'] = 1.5
 
-parameters['Rayleigh'] = 1e8
+parameters['Rayleigh'] = 1e7
 parameters['Prandtl'] = 0.5
 parameters['Taylor'] = 1
 parameters['tau'] = parameters['Prandtl']
-parameters['tau_bg'] = 1e-3
-parameters['inv_R'] = 3
+parameters['tau_bg'] = 3e-3
+parameters['inv_R'] = 10
 parameters['dealias'] = 3/2
-parameters['stop_sim_time'] = 300
+parameters['stop_sim_time'] = 10000
 
